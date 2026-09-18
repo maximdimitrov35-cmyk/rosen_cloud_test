fe8741603800f69a4cbe449b7459e255de8ac9ce
maximdimitrov35-cmyk <maximdimitrov35@gmail.com>
2026-09-18 13:02:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,33 @@ client = genai.Client(api_key=api_key)
 
 st.title("Росен AI")
 st.caption("v2.5 Cloud Test")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-user_message = st.text_input("Message Росен")
+user_message = st.chat_input("Message Росен")
 
-if st.button("Send") and user_message:
-    with st.spinner("Росен is thinking..."):
-        response = client.models.generate_content(
-            model="gemma-4-26b-a4b-it",
-            contents=user_message
-        )
+if user_message:
+    st.session_state.messages.append({
+        "role": "user",
+        "content": user_message
+    })
 
-    st.write(response.text)
+    with st.chat_message("user"):
+        st.write(user_message)
+
+    with st.chat_message("assistant"):
+        with st.spinner("Росен is thinking..."):
+            response = client.models.generate_content(
+                model="gemma-4-26b-a4b-it",
+                contents=user_message
+            )
+
+        st.write(response.text)
+
+    st.session_state.messages.append({
+        "role": "assistant",
+        "content": response.text
+    })
