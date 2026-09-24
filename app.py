1d1014ceb5b7c9f2c36e8db7b8f56508c57c3328
maximdimitrov35-cmyk <maximdimitrov35@gmail.com>
2026-09-24 16:47:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -667,8 +667,7 @@ def generate_image(prompt, status_box):
         )
 
     # Wait for the volunteer workers to finish.
-       for _ in range(90):
-
+        for _ in range(90):
             time.sleep(2)
 
     status_response = requests.get(
